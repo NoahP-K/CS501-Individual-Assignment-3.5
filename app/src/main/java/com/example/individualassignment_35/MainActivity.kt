bcc19762703c39b329dd4873a3edf0225ce6241c
NoahP-K <noahpk@bu.edu>
2025-02-09 11:15:32 -0500

    Updated design to utilize dialog box.

--- app/src/main/java/com/example/individualassignment_35/MainActivity.kt
+++ app/src/main/java/com/example/individualassignment_35/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.individualassignment_35
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Gravity
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -32,17 +33,18 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogWindowProvider
 import com.example.individualassignment_35.ui.theme.IndividualAssignment_35Theme
 import kotlinx.coroutines.launch
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -134,20 +136,20 @@ fun assertBoard(board: Array<Array<MutableState<Cell>>>): Boolean {
     for(sGridRow in 0..2) {
         for(row in 0..2) {
             if (!assertRowCol(
-                getSubRow(board[sGridRow*3 + 0], row),
-                getSubRow(board[sGridRow*3 + 1], row),
-                getSubRow(board[sGridRow*3 + 2], row)
-            )) return false
+                    getSubRow(board[sGridRow*3 + 0], row),
+                    getSubRow(board[sGridRow*3 + 1], row),
+                    getSubRow(board[sGridRow*3 + 2], row)
+                )) return false
         }
     }
     //check all columns
     for(sGridCol in 0..2) {
         for(col in 0..2) {
             if(!assertRowCol(
-                getSubCol(board[sGridCol + 0], col),
-                getSubCol(board[sGridCol + 3], col),
-                getSubCol(board[sGridCol + 6], col)
-            )) return false
+                    getSubCol(board[sGridCol + 0], col),
+                    getSubCol(board[sGridCol + 3], col),
+                    getSubCol(board[sGridCol + 6], col)
+                )) return false
         }
     }
     return true
@@ -179,6 +181,27 @@ class Cell(selected: MutableState<Cell?>,
         var boxColor = if(!mutable) Color.LightGray
             else if (s.value == this@Cell) Color(0xFFD4E1A6)
             else Color.White
+        var show = remember {mutableStateOf(false)}
+        if(show.value) {
+            Dialog(
+                onDismissRequest = {
+                    show.value = false
+                    s.value = null
+                }
+            ){
+                //-------------
+                //This block of code I found online. I was looking for a way to shift
+                // the position of the dialog box and remove the screen dim and it did
+                // the job.
+                val dialogWindowProvider = LocalView.current.parent as? DialogWindowProvider
+                dialogWindowProvider?.window?.let { window ->
+                    window.setGravity(Gravity.BOTTOM)   //set dialog to bottom of screen
+                    window.setDimAmount(0f)     //set screen dim value to 0
+                }
+                //-------------
+                buttonDialog(s)
+            }
+        }
         Box(
             modifier = Modifier
                 .background(color = boxColor)
@@ -189,7 +212,10 @@ class Cell(selected: MutableState<Cell?>,
                     //When clicked, the cell either sets itself as the selected cell
                     // or, if it is already selected, unselects itself.
                     //Retains current selected cell if the current cell is not mutable.
-                    s.value = if(!mutable) s.value else if (s.value == this@Cell) null else this@Cell
+                    s.value = if(!mutable) s.value
+                        else if (s.value == this@Cell) null
+                        else this@Cell
+                    show.value = true
                 }
         ) {
             Text(
@@ -200,6 +226,42 @@ class Cell(selected: MutableState<Cell?>,
                 modifier = Modifier.align(Alignment.Center)
             )
         }
+    }
+}
+
+//make a dialog with button options for the user to pick
+@Composable
+fun buttonDialog(selectedCell: MutableState<Cell?>) {
+    //Make a collection of buttons used to modify the cells of the puzzle
+    Card() {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(5),
+            contentPadding = PaddingValues(6.dp),
+            content = {
+                val buttons = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+                items(buttons.size) { ind ->
+                    Button(
+                        shape = RoundedCornerShape(10.dp),
+                        modifier = Modifier
+                            .padding(6.dp)
+                            .size(width = 100.dp, height = 50.dp),
+                        onClick = {
+                            selectedCell.value?.setV(buttons[ind])
+                        }
+                    ) {
+                        Text(
+                            text = if (buttons[ind] != 0) buttons[ind].toString() else "X",
+                            fontSize = 24.sp,
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+
+                        )
+                    }
+                }
+            }
+        )
     }
 }
 
@@ -222,7 +284,7 @@ fun makeScreen(initialBoard: List<Int>? = null){
     val selectedCell: MutableState<Cell?> = remember {mutableStateOf(null)}
     //the actual set of values in the grid
     val subGrids: Array<Array<MutableState<Cell>>>
-    = remember { Array(9) { Array(9) { mutableStateOf( Cell(selectedCell) )}} }
+            = remember { Array(9) { Array(9) { mutableStateOf( Cell(selectedCell) )}} }
 
     //set the initial immutable values
     var countSubGrid = 0
@@ -268,32 +330,6 @@ fun makeScreen(initialBoard: List<Int>? = null){
                     }
                 }
             )
-            //Make a collection of buttons used to modify the cells of the puzzle
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(5),
-                contentPadding = PaddingValues(25.dp),
-                content = {
-                    val buttons = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
-                    items(buttons.size) { ind ->
-                        Button(
-                            shape = RoundedCornerShape(10.dp),
-                            modifier = Modifier
-                                .padding(6.dp),
-                            onClick = {
-                                selectedCell.value?.setV(buttons[ind])
-                            }
-                        ){
-                            Text(
-                                text = if(buttons[ind] != 0) buttons[ind].toString() else "X",
-                                fontSize = 16.sp,
-                                color = Color.White,
-                                textAlign = TextAlign.Center,
-                                modifier = Modifier.align(Alignment.CenterVertically)
-                            )
-                        }
-                    }
-                }
-            )
             //A button to clear the full board
             Button(
                 onClick = {
@@ -320,6 +356,11 @@ fun makeScreen(initialBoard: List<Int>? = null){
 @Composable
 fun preview() {
     IndividualAssignment_35Theme {
-        makeScreen()
+        //makeScreen()
+        Dialog(
+            onDismissRequest = {}
+        ) {
+            buttonDialog(remember { mutableStateOf(null) })
+        }
     }
 }
